71c9a96c9f1e7ec8bbffdbc23bce04c1d66259bb
smazcw3 <arshadmail99_zaidi@yahoo.co.in>
2017-01-08 18:50:01 -0500

    Committing Inheritance I and Java Loops I Problem solution on 08/01/2017 -1850EST

--- HackerRank - Java Inheritance I/src/Solution.java
+++ HackerRank - Java Inheritance I/src/Solution.java
@@ -1,11 +1,8 @@
-public class Solution{
-
+class Solution{
    public static void main(String args[]){
-
 	  Bird bird = new Bird();
 	  bird.walk();
 	  bird.fly();
-      bird.sing();
-	
+      bird.sing();	
    }
 }
