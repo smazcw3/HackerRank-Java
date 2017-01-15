f583ea44d8fb4f80763dc7371dace01a05962013
Syed Mohammed Arshad Zaidi <arshadmail99_zaidi@yahoo.co.in>
2017-01-15 17:31:05 -0500

    Update Solution.java

--- HackerRank - Java Date and Time/src/Solution.java
+++ HackerRank - Java Date and Time/src/Solution.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int month = in.nextInt();
-        int day = in.nextInt();
-        int year = in.nextInt();
-        LocalDate date = LocalDate.of(year, month, day);
-        System.out.print(date.getDayOfWeek());
-        in.close();
+        	Scanner in = new Scanner(System.in);
+        	int month = in.nextInt();
+		int day = in.nextInt();
+		int year = in.nextInt();
+		LocalDate date = LocalDate.of(year, month, day);
+		System.out.print(date.getDayOfWeek());
+		in.close();
 	}
 }
