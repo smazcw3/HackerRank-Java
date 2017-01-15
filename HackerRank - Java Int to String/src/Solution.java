e2d1e9fa01482283cc5ed005929654e65a8339fa
smazcw3 <arshadmail99_zaidi@yahoo.co.in>
2017-01-15 14:32:44 -0500

    Committing int to string code 15/01/2017 -1432EST

---
+++ HackerRank - Java Int to String/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+
+		DoNotTerminate.forbidExit();
+
+		  try {
+		   Scanner in = new Scanner(System.in);
+		   int n = in .nextInt();
+		   in.close();
+		   String s = Integer.toString(n);
+		   if (n == Integer.parseInt(s)) {
+		    System.out.println("Good job");
+		   } else {
+		    System.out.println("Wrong answer.");
+		   }
+		  } catch (DoNotTerminate.ExitTrappedException e) {
+		   System.out.println("Unsuccessful Termination!!");
+		  }
+		 }
+		}
