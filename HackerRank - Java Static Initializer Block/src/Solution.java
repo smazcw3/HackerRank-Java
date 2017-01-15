e7c3251675ecdb4e941831b35b30b2244458b293
smazcw3 <arshadmail99_zaidi@yahoo.co.in>
2017-01-15 15:08:01 -0500

    Committing static initializer Block code 15/01/2017 -1508EST

---
+++ HackerRank - Java Static Initializer Block/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+    static boolean flag;
+	static int B = sc.nextInt();
+	static int H = sc.nextInt();
+	static{
+		if(B > 0 && H > 0){
+			flag = true;
+		}
+		else{
+			try{
+				flag = false;
+				throw new Exception("Breadth and height must be positive");
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+		sc.close();
+	}
+	
+	
+	public static void main(String[] args){
+		if(flag){
+			int area = B * H;
+			System.out.print(area);
+		}		
+	}//end of main
+
+}//end of class
+
