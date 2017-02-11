3dab2efa04394dd44412cbed010984f0479096f7
Syed Mohammed Arshad Zaidi <arshadmail99_zaidi@yahoo.co.in>
2017-02-10 22:35:37 -0500

    Updated code

--- HackerRank - Java 2D Array/src/Solution.java
+++ HackerRank - Java 2D Array/src/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 
     public static void main(String[] args) {
         @SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
         int arr[][] = new int[6][6];
         for(int i=0; i < 6; i++){
             for(int j=0; j < 6; j++){
