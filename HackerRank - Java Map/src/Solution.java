edd3bc5b17e22c1c80c3074b68551979aba96916
Syed Mohammed Arshad Zaidi <arshadmail99_zaidi@yahoo.co.in>
2017-05-22 21:08:41 -0400

    Update Solution.java

--- HackerRank - Java Map/src/Solution.java
+++ HackerRank - Java Map/src/Solution.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 class Solution {
-
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
